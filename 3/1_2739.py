363cb49ea546bd3a8ae21a8be7dec405825c93e3
juone00 <juwon0208@naver.com>
2022-08-30 22:55:10 +0900

    ~3_2_10950

---
+++ 3/1_2739.py
@@ -0,0 +1,6 @@
+a = input()
+a = int(a)
+
+for i in range(1,10):
+    result = i * a
+    print(f"{a} * {i} = {result}")
---
+++ 3/2_10950.py
@@ -0,0 +1,8 @@
+a = int(input())
+
+for i in range(0,a):
+    b,c = input().split()
+    b = int(b)
+    c = int(c)
+    
+    print(b+c)
