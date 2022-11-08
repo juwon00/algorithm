4ea86b05defbb62f419389148cb4e23f18202f56
juone00 <juwon0208@naver.com>
2022-11-09 01:36:10 +0900

    ~27_11_7562

--- 27/11_7562.py
+++ 27/11_7562.py
@@ -6,24 +6,22 @@ dy = [2, 1, -1, -2, -2, -1, 1, 2]
 def bfs():
     q = deque()
     q.append([x,y]) # 입력받은 x,y를 한번에 append함
-    move[x][y] = 1
+    graph[x][y] = 1 # 위치에 1증가시킴
     
     while q:
-        a,b = q.popleft()
+        a,b = q.popleft() # (ex: 처음 pop은 x,y , 그 이후는 8방향 ...) 
         
-        if a == goal_x and b == goal_y:
-            print(move[goal_x][goal_y] - 1)
+        if a == goal_x and b == goal_y: # 목표지점에 도착시 끝
+            print(graph[goal_x][goal_y] - 1) # 처음부터 1로 시작하므로 1을 빼고 출력
             return 
         
-        for i in range(8):
+        for i in range(8): #8방향으로 bfs
             nx = a + dx[i]
             ny = b + dy[i]
             
-            if 0 <= nx < l and 0 <= ny < l and move[nx][ny] == 0:
+            if 0 <= nx < l and 0 <= ny < l and graph[nx][ny] == 0: # 범위안에서 안가본 곳일 때
                 q.append([nx, ny])
-                move[nx][ny] = move[a][b] + 1
-
-
+                graph[nx][ny] = graph[a][b] + 1 # 위치에 1증가시킴
 
 
 T = int(input())
@@ -34,11 +32,6 @@ for _ in range(T):
     x,y = map(int, input().split())
     goal_x, goal_y = map(int, input().split())
     
-    move = [[0]*l for _ in range(l)]
-    bfs()
-
-
-
-    
-    
+    graph = [[0]*l for _ in range(l)] # 그래프 초기화
+    bfs()    
     
