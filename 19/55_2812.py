2a13793f134cca0b53afe3037f4f44c42c92b419
juone00 <juwon0208@naver.com>
2022-11-23 15:05:39 +0900

    ~19_55_2812

--- 19/55_2812.py
+++ 19/55_2812.py
@@ -10,12 +10,12 @@ stack = list()
 #! 아래 주석은 내 코드 시간초과가 난다 왜??
 # for i in num:
 
-#     if not stack:
+#     if not stack: # 처음에 스택에 아무것도 없을 때 실행
 #         stack.append(i)
 #         continue
     
-#     for j in reversed(stack):
-#         if j < i and k > 0:
+#     for j in reversed(stack): # 넣을 수 보다 앞에 작은수가 있으면 빼는것이 핵심
+#         if j < i and k > 0: # 스택안에 넣을 수 보다 작은 것이 있고 빼는 횟수를 초과하지 않았을 때
 #             k -= 1
 #             stack.pop()
 
