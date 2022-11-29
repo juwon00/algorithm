503a4cf25785ad67c8157ffefe88f315ba1e59a9
juone00 <juwon0208@naver.com>
2022-11-30 01:18:59 +0900

    ~27_54_2636

--- 27/54_2636.py
+++ 27/54_2636.py
@@ -1,44 +1,50 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-def bfs(x,y): # 바깥부분만 탐색하도록
+def bfs(): 
     q = deque()
-    q.append([x,y])
-    graph[x][y] = hours
+    q.append([0,0]) # 0,0부터 시작
+    visited[0][0] = 1
+    cnt = 0
     
+    while q:
+        x,y = q.popleft()
+        for i in range(4): # 동서남북 4방향 탐색
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0<=nx<X and 0<=ny<Y and visited[nx][ny] == 0: # 범위안에서 방문안한곳이 있을 때
+                if graph[nx][ny] == 0: # 그곳이 0이라면 큐에 넣고 방문처리
+                    visited[nx][ny] = 1
+                    q.append([nx,ny])
+                    
+                elif graph[nx][ny] == 1: #* 그곳이 1이라면 큐에 넣지 않고 방문처리한다 이때 1은 가장자리 치즈임을 알 수 있다
+                    graph[nx][ny] = 0
+                    visited[nx][ny] = 1
+                    cnt += 1
+    ans.append(cnt)
+    return cnt
+
 
 
 X, Y = map(int, input().split())
-
 graph = []
 for i in range(X):
     graph.append(list(map(int, input().split())))
     
-hours = 2
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+ans = []
 
-while graph in 1:
-    
-    last_cheeze = 0
-    
-    bfs(0, 0)
-    
-    for i in range(X):
-        for j in range(Y):
-            if graph[i][j] == 1:
-                for k in range(4):
-                    nx = i + dx[k]
-                    ny = j + dy[k]
-                    
-                    if graph[nx][ny] == hours:
-                        last_cheeze += 1
-                        graph[i][j] = 0
-                        break
-    # 그래프에 있는 모든 hours를 0으로 바꾸고
+hours = 0 # 치즈가 녹아 없어지는 시간
+while 1:
     hours += 1
-    
-print(hours-1)
-print(last_cheeze)
+
+    visited = [[0]*Y for _ in range(X)]    
+    cnt = bfs()
+    if cnt == 0:
+        break
+
+print(hours - 1)
+print(ans[-2])
