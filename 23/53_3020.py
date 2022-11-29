25ec68f47b96e86887bad92464bf5a8c36696bda
juone00 <juwon0208@naver.com>
2022-11-30 02:29:27 +0900

    [수정]23_52_3020

--- 23/53_3020.py
+++ 23/53_3020.py
@@ -3,3 +3,13 @@ obstacle = []
 for _ in range(N):
     obstacle.append(int(input()))
     
+start = 0
+end = H
+
+result = 0
+while start <= end:
+    total = 0
+    mid = (start+end) // 2
+    
+    for x in obstacle:
+        
