995a4e0f7da7c7188a3a2ced53c5cdbd8005f4bf
juone00 <juwon0208@naver.com>
2022-11-30 15:54:32 +0900

    ~23_53_3020

--- 23/53_3020.py
+++ 23/53_3020.py
@@ -1,15 +1,42 @@
+# - 다른분 풀이 참고한 문제 -
 N, H = map(int, input().split())
 obstacle = []
 for _ in range(N):
     obstacle.append(int(input()))
     
-start = 0
-end = H
+bottom = []
+top = []
 
-result = 0
-while start <= end:
-    total = 0
-    mid = (start+end) // 2
-    
-    for x in obstacle:
+for i in range(N):
+    if i%2 == 0:
+        bottom.append(obstacle[i])
+    else:
+        top.append(obstacle[i])
         
+top.sort()
+bottom.sort()
+
+result = N
+count = 0
+
+def binary(array, target, start, end):
+    while start <= end:
+        mid = (start+end) // 2
+        
+        if array[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
+
+for i in range(1, H+1):
+    bottom_count = len(bottom) - binary(bottom, i-0.5, 0, len(bottom)-1)
+    top_count = len(top) - binary(top, H-i+0.5, 0, len(top)-1)
+    
+    if result == bottom_count + top_count:
+        count += 1
+    elif result > bottom_count + top_count:
+        count = 1
+        result = bottom_count + top_count
+
+print(result, count)
