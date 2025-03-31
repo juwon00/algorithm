083138d0337ea07a74996ac5d175e8f3f3c22b46
juone00 <juwon0208@naver.com>
2025-03-31 22:14:10 +1000

    BOJ: 감소하는_수 [python]
     - 감소하는_수: brute-force? (combinations, 백트래킹도 가능)

---
+++ baekjoon/17_implementation/1038_감소하는_수.py
@@ -0,0 +1,24 @@
+# 다시 볼 문제 - 시간초과 (combinations, 백트래킹 방법으로)
+
+def get_decreasing_number(N):
+    if N >= 1023:  # 감소하는 수는 최대 1023개 존재
+        return -1
+
+    count = 0
+    num = 0
+
+    while True:
+        if is_decreasing(num):  # 감소하는 수인지 확인
+            count += 1
+            if count == N + 1:  # N번째 감소하는 수 찾기
+                return num
+        num += 1
+
+
+def is_decreasing(n):
+    str_n = str(n)
+    return all(str_n[i] > str_n[i + 1] for i in range(len(str_n) - 1))
+
+
+n = int(input())
+print(get_decreasing_number(n))
